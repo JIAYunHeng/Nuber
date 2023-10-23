054ab54c907ad74b76eea4ae9dc78ff24ac03c41
JIAYunHeng <jiayy070@qq.com>
2023-10-23 19:14:07 +0800

    feat:Booking

--- src/nuber/students/Booking.java
+++ src/nuber/students/Booking.java
@@ -1,5 +1,6 @@
 package nuber.students;
-
+import java.util.Date;
+import java.lang.StringBuffer;
 /**
  * 
  * Booking represents the overall "job" for a passenger getting to their destination.
@@ -19,8 +20,17 @@ package nuber.students;
  *
  */
 public class Booking {
+	static Integer autoIncrementID = 0;
+	private int bookingID;
+	private NuberDispatch dispatch;
+	private Passenger passenger;
+	private Driver driver;
 
-		
+	public synchronized int addAndGetBookingID(){
+		bookingID += 1;
+		return bookingID;
+	}
+
 	/**
 	 * Creates a new booking for a given Nuber dispatch and passenger, noting that no
 	 * driver is provided as it will depend on whether one is available when the region 
@@ -29,8 +39,10 @@ public class Booking {
 	 * @param dispatch
 	 * @param passenger
 	 */
-	public Booking(NuberDispatch dispatch, Passenger passenger)
-	{
+	public Booking(NuberDispatch dispatch, Passenger passenger) {
+		this.dispatch = dispatch;
+		this.passenger = passenger;
+		this.bookingID = addAndGetBookingID();
 	}
 	
 	/**
@@ -43,14 +55,37 @@ public class Booking {
 	 * 4.	It must then call the Driver.driveToDestination() function, with the thread pausing 
 	 * 			whilst as function is called.
 	 * 5.	Once at the destination, the time is recorded, so we know the total trip duration. 
-	 * 6.	The driver, now free, is added back into Dispatch’s list of available drivers. 
+	 * 6.	The driver, now free, is added back into Dispatchï¿½s list of available drivers. 
 	 * 7.	The call() function the returns a BookingResult object, passing in the appropriate 
 	 * 			information required in the BookingResult constructor.
 	 *
 	 * @return A BookingResult containing the final information about the booking 
 	 */
-	public BookingResult call() {
-
+	public BookingResult call() throws InterruptedException {
+		//1 ask for driver
+		//2 wait while no dirver
+		this.driver = dispatch.getDriver();// may wait in the function
+		//3 pickUpPassenger
+		Date beginDate = new Date();
+		long begin = beginDate.getTime();
+		long tripDuration = 0;
+		try {
+			driver.pickUpPassenger(passenger);
+			//4 driveToDestination
+			driver.driveToDestination();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//5 record time
+		Date endDate = new Date();
+		long end = endDate.getTime();
+		tripDuration = end - begin;
+		//6 free driver
+		dispatch.addDriver(driver);
+		//7 return bookingResult;
+		BookingResult bookingResult = new BookingResult(bookingID, passenger, driver, tripDuration);
+		return bookingResult;
 	}
 	
 	/***
@@ -64,8 +99,14 @@ public class Booking {
 	 * @return The compiled string
 	 */
 	@Override
-	public String toString()
-	{
+	public String toString() {
+		StringBuffer str = new StringBuffer("");
+		str.append(bookingID)
+				.append(":")
+				.append(driver == null ? "null" : driver.name)
+				.append(":")
+				.append(passenger == null ? "null" : passenger.name);
+		return str.toString();
 	}
 
 }
